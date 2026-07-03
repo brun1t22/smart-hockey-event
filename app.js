2e235720cc2ea359453c80d460e4bef35c51f81d
brun1t22 <brunotallavia@gmail.com>
2026-07-03 16:11:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Configuración de Firebase (ya la tenés)
 const firebaseConfig = {
-  apiKey: "AIzaSyA3XqliF4oALMuuKFhtzEpE_7-eIm0jGQc",
+  apiKey: "TU_API_KEY",
   authDomain: "smarthockey-3aebd.firebaseapp.com",
   projectId: "smarthockey-3aebd",
   storageBucket: "smarthockey-3aebd.firebasestorage.app",
   messagingSenderId: "560324583192",
-  appId: "1:560324583192:web:0dc9593181df61f822c86d",
+  appId: "1:560324583192:web:0dc95931814d6f1822c864",
   measurementId: "G-VBMY3G0RMG"
 };
 
-// Initialize Firebase
+// Inicializar Firebase y Firestore
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
